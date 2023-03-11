3e1bd9a4f344d3cef09ad4d20dafc1d12f7f0457
Gustavo Henrique dos Santos <gustavohenrique.santos97@gmail.com>
2023-03-11 11:25:25 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dsclientes/DsclientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsclientes/DsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
