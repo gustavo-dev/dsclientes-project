a11fac678db17a959ba100b23ba7609aebc49596
Gustavo Henrique dos Santos <gustavohenrique.santos97@gmail.com>
2023-03-19 15:13:06 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dsclientes/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclientes/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsclientes.entities.Client;
 import com.devsuperior.dsclientes.repositories.ClientRepository;
@@ -14,9 +15,8 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly= true)
 	public List<Client> findAll() {
 		return repository.findAll();
-
 	}
-
 }
